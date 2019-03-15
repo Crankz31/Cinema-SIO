3fb5d5478b99df693c4f94e06eb980c1d412e970
Crankz31 <47506142+Crankz31@users.noreply.github.com>
2019-03-15 16:41:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     $host = "127.0.0.1";
     $user = "root";                     //Your Cloud 9 username
-    $pass = "";                                  //Remember, there is NO password by default!
+    $pass = "";                                  //il n'y a pas de mot de passe par default
     $db = "db_movie";                                  //Your database name you want to connect to
     $port = 3306;
      $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
