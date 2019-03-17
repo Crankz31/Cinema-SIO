0ee3e48a5b80fee83c8c8294b8ee776e5da191bc
Alexdu65200 <AlexandreOliva65@gmail.com>
2019-03-17 22:28:46 +0100

    TRAD

--- footer.php
+++ footer.php
@@ -4,32 +4,30 @@
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="footer-nav">
 		                <ul>
-		                   <li><a href="index.php">Home</a></li>
-			  		   <li><a href="movies_events.php">Movies</a></li>
-			  		   <li><a href="login.php">Login</a></li>
+		                   <li><a href="index.php">Accueil</a></li>
+			  		   <li><a href="movies_events.php">Films à l'affiche</a></li>
+			  		   <li><a href="login.php">Connexion</a></li>
 		                   </ul>
 		              </div>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="textcontact">
-						<p>Theatre Assistance<br>
-						Theatre 3D Movie<br>
-						Ph: +1-800-234-52589.<br>
+						<p>GOMB CINEMA<br>
+						2 Rue Georges Ledormeur<br>
+						65000 Tarbes<br>
 						</p>
 					</div>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="call_info">
-						<p class="txt_3">Call us toll free:</p>
-						<p class="txt_4">1 800 234 23456</p>
+						<p class="txt_3">Appelez nous:</p>
+						<p class="txt_4">0562910102</p>
 					</div>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<div class=social>
 						<a href="#"><img src="images/fb.png" alt=""/></a>
 						<a href="#"><img src="images/tw.png" alt=""/></a>
-						<a href="#"><img src="images/dribble.png" alt=""/></a>
-						<a href="#"><img src="images/pinterest.png" alt=""/></a>
 					</div>
 				</div>
 				<div class="clear"></div>
