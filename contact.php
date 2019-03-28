2cdc0d1709ca1bb5a55664c87c5e45e8be8ef76d
StrNe <48594958+StrNe@users.noreply.github.com>
2019-03-28 14:08:00 +0100

    ModifAffichage
    Modification index et page contact

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Free Theater Website Template | Contact :: w3layouts</title>
+<title>PROJET CINEMA SIO</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js'></script>
@@ -23,48 +23,25 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		</div>
 			  <div class="nav-wrap">
 					<ul class="group" id="example-one">
-			           <li><a href="index.php">Home</a></li>
-			           <li><a href="about.php">About</a></li>
-			  		   <li><a href="movies_events.php">Movies & Events</a></li>
+			           <li><a href="index.php">ACCUEIL</a></li>
+			           <li><a href="about.php">A PROPOS</a></li>
+			  		   <li><a href="movies_events.php">FILMS ET EVENEMENTS</a></li>
 			  		   <li class="current_page_item"><a href="contact.php">Contact</a></li>
 			        </ul>
 			  </div>
  			<div class="clear"></div>
    		</div>
     </div>
-<div class="block">
-	<div class="wrap">
-		
-        <form action="#" id="reservation-form" method="post">
-		       <fieldset>
-		                <div class="field">
-			                 <label>Find a Movie:</label>
-			                  <select class="select2">
-			                    <option>Movie list</option>
-			                  </select>
-			            </div>
-		                <div class="field1">
-			                   <label>Search</label>
-			                  <select class="select1">
-			                    <option>Movies or Actors</option>
-			                  </select>
-		                </div>
-		       </fieldset>
-            </form>
-            <div class="clear"></div>
-   </div>
-</div>
-</div>
 <div class="content">
 	<div class="wrap">
 		<div class="content-top">
 				<div class="section group">
 				<div class="col span_2_of_3">
 				  <div class="contact-form">
-				  	<h3>Contact Us</h3>
+				  	<h3>Nous contacter</h3>
 					    <form action="process_contact.php" method="post" name="form11">
 					    	<div>
-						    	<span><label>NAME</label></span>
+						    	<span><label>Nom</label></span>
 						    	<span><input type="text" value="" required name="name"></span>
 						    </div>
 						    <div>
@@ -72,16 +49,14 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						    	<span><input type="text" value="" required name="email"></span>
 						    </div>
 						    <div>
-						     	<span><label>MOBILE.NO</label></span>
+						     	<span><label>Tel.NO</label></span>
 						    	<span><input type="number" value="" required name="mobile"></span>
 						    </div>
 						    <div>
-						    	<span><label>SUBJECT</label></span>
+						    	<span><label>SUJET</label></span>
 						    	<span><textarea required name="subect"> </textarea></span>
 						    </div>
-						   <div>
-						   		<span><input type="submit">Submit</span>
-						  </div>
+						   
 					    </form>
 				  </div>
   				</div>
@@ -89,17 +64,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<div class="contact_info">
     	 				<h3>Find Us Here</h3>
 					    	  <div class="map">
-							   	    <iframe width="100%" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265&amp;output=embed"></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#666;text-align:left;font-size:12px">View Larger Map</a></small>
+							   	    <iframe width="100%" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.3921946187406!2d0.07149691586548569!3d43.22223617913848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a9d4777d5b229f%3A0x9f7d1c13ad55b206!2sLyc%C3%A9e+G%C3%A9n%C3%A9ral+et+Technologique+Marie+Curie!5e0!3m2!1sfr!2sfr!4v1553774177112" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#666;text-align:left;font-size:12px">View Larger Map</a></small>
 							  </div>
       				</div>
       			<div class="company_address">
-				     	<h3>Company Information :</h3>
-						    	<p>500 Lorem Ipsum Dolor Sit,</p>
-						   		<p>22-56-2-9 Sit Amet, Lorem,</p>
-						<p>Phone:(00) 222 666 444</p>
-				   		<p>Fax: (000) 000 00 00 0</p>
-				 	 	<p>Email: <span>info@mycompany.com</span></p>
-				   		<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
+				     	<h3>Information Société :</h3>
+						    	<p>2 Rue George Ledormeur</p>					   		
+						<p>Phone: 05.62.91.01.02</p>
 				   </div>
 				 </div>
 			  </div>
@@ -113,34 +84,26 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="footer-nav">
 		                <ul>
-		                   <li><a href="">Our Tips of gallery Template diam</a></li>
-		                    <li><a href="">Our Tips of gallery Template diam</a></li>
-		                     <li><a href="">Our Tips of gallery Template diam</a></li>
-		                       <li><a href="">Our Tips of gallery Template diam</a></li>
+		                   <li><a href="index.php">Accueil</a></li>
+		                    <li><a href="movies_events.php">Films à l'affiche</a></li>
+		                     <li><a href="login.php">Connexion</a></li>
+		                      
 		                   </ul>
 		              </div>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="textcontact">
-						<p>consectetuer adipiscing elit,<br>
-						consectetuer<br>
-						Ph: +1-800-234-52589.<br>
-						Email:Support(at)Fotoria-scope.com<br>
+						<p>GOMB CINEMA<br>
+						2 Rue George Ledormeur<br>
+						65000 Tarbes<br>
+					
 						</p>
 					</div>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<div class="call_info">
-						<p class="txt_3">Call us toll free:</p>
-						<p class="txt_4">1 800 234 23456</p>
-					</div>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<div class=social>
-						<a href="#"><img src="images/fb.png" alt=""/></a>
-						<a href="#"><img src="images/tw.png" alt=""/></a>
-						<a href="#"><img src="images/dribble.png" alt=""/></a>
-						<a href="#"><img src="images/pinterest.png" alt=""/></a>
+						<p class="txt_3">Appelez nous:</p>
+						<p class="txt_4">0562910102</p>
 					</div>
 				</div>
 				<div class="clear"></div>
